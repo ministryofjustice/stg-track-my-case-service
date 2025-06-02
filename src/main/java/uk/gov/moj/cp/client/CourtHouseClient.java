55341902322ce9c965e5c71d2484e06f4cd6844a
Dmitri Algazin <Dmitri.Algazin1@justice.com>
2025-06-02 15:29:37 +0100

    feat: moving any hardcoded URLs under application properties, covering with tests;

--- src/main/java/uk/gov/moj/cp/client/CourtHouseClient.java
+++ src/main/java/uk/gov/moj/cp/client/CourtHouseClient.java
@@ -1,42 +1,63 @@
 package uk.gov.moj.cp.client;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CourtHouseClient {
-    private final String courtHouseurl = "https://virtserver.swaggerhub.com/HMCTS-DTS/api-cp-refdata-courthearing-courthouses/0.4.0/courthouses/%s";
 
-    private static final HttpHeaders headers = new HttpHeaders();
     private final RestTemplate restTemplate;
 
-    @Autowired
-    public CourtHouseClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    @Value("${services.courthearing-courthouses.url}")
+    private String courthearingCourthousesUrl;
+
+    @Value("${services.courthearing-courthouses.version}")
+    private String getCourthearingCourthousesVersion;
+
+    private static final String COURTHOUSES_BY_ID = "courthouses/{id}";
+
+    protected String getCourthearingCourthousesById(String id) {
+        return UriComponentsBuilder
+            .fromUri(URI.create(courthearingCourthousesUrl))
+            .pathSegment(getCourthearingCourthousesVersion)
+            .pathSegment(COURTHOUSES_BY_ID)
+            .buildAndExpand(id)
+            .toUriString();
     }
 
-    public HttpEntity<String> getCourtHouseById(String id) {
-        headers.set("Accept", "application/json");
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        ResponseEntity<String> response = null;
+    public ResponseEntity<String> getCourtHouseById(String id) {
         try {
-            response = restTemplate.exchange(
-                String.format(courtHouseurl, id),
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                getCourthearingCourthousesById(id),
                 HttpMethod.GET,
-                entity,
+                getRequestEntity(),
                 String.class
             );
+            return responseEntity;
         } catch (Exception e) {
             log.error("Error while calling CourtHouse API", e);
         }
-        return response;
+        return null;
+    }
+
+    protected HttpEntity<String> getRequestEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return new HttpEntity<>(headers);
     }
 
 }
