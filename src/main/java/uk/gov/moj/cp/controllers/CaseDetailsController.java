4a9133fc094b9a2a5773f36e0e3256fa499dc8ad
Munish Jalota <mjalota@gmail.com>
2025-10-08 12:23:01 +0100

    Added output message (#75)

--- src/main/java/uk/gov/moj/cp/controllers/CaseDetailsController.java
+++ src/main/java/uk/gov/moj/cp/controllers/CaseDetailsController.java
@@ -1,12 +1,11 @@
 package uk.gov.moj.cp.controllers;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.moj.cp.dto.CaseDetailsDto;
 import uk.gov.moj.cp.service.CaseDetailsService;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -16,7 +15,6 @@ import static org.springframework.http.ResponseEntity.ok;
  * Default endpoints per application.
  */
 @RestController
-@Slf4j
 public class CaseDetailsController {
 
     /**
@@ -29,9 +27,14 @@ public class CaseDetailsController {
     private CaseDetailsService caseDetailsService;
 
     @GetMapping("/case/{case_urn}/casedetails")
-    public ResponseEntity<CaseDetailsDto> getCaseDetailsByCaseUrn(@PathVariable("case_urn") String caseUrn) {
-        log.atInfo().log("Received request to get case details for caseUrn: {}", caseUrn);
-        return ok(caseDetailsService.getCaseDetailsByCaseUrn(caseUrn));
+    public ResponseEntity<?> getCaseDetailsByCaseUrn(@PathVariable("case_urn") String caseUrn) {
+        try {
+            return ok(caseDetailsService.getCaseDetailsByCaseUrn(caseUrn));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("An error occurred while processing the request either caseUrn is not available. "
+                          + "or see the logs for more details.");
+        }
     }
 
 }
