7419e6d4622a20c8b2ac8960e009595166944bc2
Dmitri Algazin <Dmitri.Algazin1@justice.com>
2025-10-20 15:12:55 +0100

    feat: hearing date validation;
    - test added;

--- src/test/java/uk/gov/moj/cp/service/CaseDetailsServiceTest.java
+++ src/test/java/uk/gov/moj/cp/service/CaseDetailsServiceTest.java
@@ -308,4 +308,78 @@ class CaseDetailsServiceTest {
         assertEquals(123, schedule.courtHouse().courtRoomDtoList().get(0).courtRoomId());
         assertEquals("CourtRoom 01", schedule.courtHouse().courtRoomDtoList().get(0).courtRoomName());
     }
+
+    @Test
+    @DisplayName("includes hearings when sitting date is today, but before now")
+    void testGetCaseDetailsByCaseUrnWithValidMultipleHearingScheduleForFutureSittingDateBeforeNow() {
+        final String caseUrn = "CASE123";
+        final String courtHouseId = randomUUID().toString();
+        final String courtRoomId = randomUUID().toString();
+        final String judgeId = randomUUID().toString();
+        final String hearingId = randomUUID().toString();
+        final String todayStartDate = LocalDateTime.now().minusHours(2).toString();
+        final String todayEndDate = LocalDateTime.now().minusHours(1).toString();
+
+
+        final CourtSittingDto todaySittingDto = new CourtSittingDto(
+                todayStartDate,
+                todayEndDate,
+                judgeId,
+                courtHouseId,
+                courtRoomId
+        );
+
+        final HearingDto hearingDto = new HearingDto(
+                hearingId,
+                "First Hearing",
+                "First Hearing",
+                "Note1",
+                List.of(todaySittingDto)
+        );
+
+        final CourtScheduleDto scheduleDto = new CourtScheduleDto(List.of(hearingDto));
+
+        final CourtRoomDto courtRoomDto = new CourtRoomDto(123, "CourtRoom 01");
+        final AddressDto addressDto = new AddressDto("53", "Court Street",
+                "London", null, "CB4 3MX", null);
+
+        final CourtHouseDto courtHouseDto = new CourtHouseDto(
+                courtHouseId,
+                courtRoomId,
+                "CROWN",
+                "123",
+                "Lavender Hill",
+                addressDto,
+                Arrays.asList(courtRoomDto)
+        );
+
+        when(courtScheduleService.getCourtScheduleByCaseUrn(caseUrn)).thenReturn(List.of(scheduleDto));
+        when(courtHouseService.getCourtHouseById(any(), any())).thenReturn(courtHouseDto);
+
+        CaseDetailsDto caseDetails = caseDetailsService.getCaseDetailsByCaseUrn(caseUrn);
+
+        assertEquals(caseUrn, caseDetails.caseUrn());
+        assertEquals(1, caseDetails.courtSchedule().size());
+        assertEquals(1, caseDetails.courtSchedule().get(0).hearings().size());
+
+        var caseHearingDetails = caseDetails.courtSchedule().get(0).hearings().get(0);
+        assertEquals(hearingId, caseHearingDetails.hearingId());
+        assertEquals("First Hearing", caseHearingDetails.hearingType());
+        assertEquals("First Hearing", caseHearingDetails.hearingDescription());
+        assertEquals("Note1", caseHearingDetails.listNote());
+
+        var schedule = caseHearingDetails.courtSittings().get(0);
+        assertEquals(judgeId, schedule.judiciaryId());
+        assertEquals(courtHouseId, schedule.courtHouse().courtHouseId());
+        assertEquals(courtRoomId, schedule.courtHouse().courtRoomId());
+        assertEquals(todayStartDate, schedule.sittingStart());
+        assertEquals(todayEndDate, schedule.sittingEnd());
+
+        assertEquals("53", schedule.courtHouse().address().address1());
+        assertEquals("Court Street", schedule.courtHouse().address().address2());
+        assertEquals("London", schedule.courtHouse().address().address3());
+
+        assertEquals(123, schedule.courtHouse().courtRoomDtoList().get(0).courtRoomId());
+        assertEquals("CourtRoom 01", schedule.courtHouse().courtRoomDtoList().get(0).courtRoomName());
+    }
 }
