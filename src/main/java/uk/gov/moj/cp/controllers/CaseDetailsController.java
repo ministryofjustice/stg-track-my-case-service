e94a2fbd682551682e8c08779a79d0a94f046ed2
Vipin Palengara <61233682+meetvipinp@users.noreply.github.com>
2025-11-10 14:26:42 +0000

    adding log (#107)

--- src/main/java/uk/gov/moj/cp/controllers/CaseDetailsController.java
+++ src/main/java/uk/gov/moj/cp/controllers/CaseDetailsController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.moj.cp.config.ApiPaths;
 import uk.gov.moj.cp.service.CaseDetailsService;
+import lombok.extern.slf4j.Slf4j;
 
 import static org.springframework.http.ResponseEntity.ok;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 @RequestMapping(ApiPaths.PATH_API_CASES)
 public class CaseDetailsController {
 
@@ -21,6 +23,7 @@ public class CaseDetailsController {
 
     @GetMapping("/{case_urn}/casedetails")
     public ResponseEntity<?> getCaseDetailsByCaseUrn(@PathVariable("case_urn") String caseUrn) {
+        log.atInfo().log("Received request to get case details for caseUrn: {}", caseUrn);
         try {
             return ok(caseDetailsService.getCaseDetailsByCaseUrn(caseUrn));
         } catch (Exception e) {
