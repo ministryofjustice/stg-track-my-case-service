7baa3f795de6c98a2bef062e23ed35bafbf93408
Vipin Palengara <Vipin.Palengara@justice.gov.uk>
2026-02-27 09:44:21 +0000

    Refactor-4

--- src/main/java/uk/gov/moj/cp/service/CaseDetailsService.java
+++ src/main/java/uk/gov/moj/cp/service/CaseDetailsService.java
@@ -21,7 +21,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.time.LocalDateTime.parse;
 import static java.util.Objects.isNull;
