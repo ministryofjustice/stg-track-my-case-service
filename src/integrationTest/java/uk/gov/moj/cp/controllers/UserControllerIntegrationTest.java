4f8dff023e77c277673c458fe6358c3b0b62d5f4
Dmitri Algazin <dmitri.algazin@hmcts.net>
2026-05-18 16:50:07 +0100

    feat: Spring boot upgrade; (#274)
    * feat: Spring boot upgrade;
    
    VP-716: [BackEnd] Upgrade Spring boot version and other related libraries to avoid vulnerabilities;
    
    ---------
    
    Co-authored-by: Dmitri Algazin <Dmitri.Algazin1@justice.com>

--- src/integrationTest/java/uk/gov/moj/cp/controllers/UserControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/moj/cp/controllers/UserControllerIntegrationTest.java
@@ -1,12 +1,12 @@
 package uk.gov.moj.cp.controllers;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
