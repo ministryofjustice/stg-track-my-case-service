551f0de3bdfef2463784e46b89ddd8602d27efc1
Dmitri Algazin <Dmitri.Algazin1@justice.com>
2026-05-27 17:53:47 +0100

    feat: building docker-compose config file to run application with mock service build in;
    VP-645: removing mock related code;

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ SERVICE_NAME="stg-track-my-case-service"
 # Check if the Docker container is running
 if [ "$(docker ps -q -f name=${SERVICE_NAME})" ]; then
     echo "Stopping the running Docker container..."
-    docker-compose down
+    docker compose down
 fi
 
 echo "Building application JAR for Docker..."
@@ -16,17 +16,17 @@ echo "Building application JAR for Docker..."
     exit 1
 }
 
-if [ ! -f docker/stg-track-my-case-service.jar ]; then
+if [ ! -f build/libs/stg-track-my-case-service.jar ]; then
     echo "ERROR: docker/stg-track-my-case-service.jar was not created. Run: ./gradlew prepareDocker"
     exit 1
 fi
 
 # Rebuild the Docker image
 echo "Rebuilding the Docker image..."
-docker-compose build
+docker compose build
 
 # Deploy the service
 echo "Starting the Docker container..."
-docker-compose up -d
+docker compose up -d
 
 echo "Deployment completed successfully."
